a0c2614db0b2e6bc85c4277e3a42e4306199c6f3
James D <james@jdrydn.com>
2014-10-15 20:43:01 +0000

    Trying to get passwords to share across SVN & Trac instances.

--- Vagrantprovision.sh
+++ Vagrantprovision.sh
@@ -8,7 +8,7 @@ locale-gen en_GB.UTF-8
 apt-get update
 # Install the first wave of packages (Apache, SVN, NodeJS, SQLite & Python Tools).
 aptitude install -y apache2 apache2.2-common apache2-mpm-prefork apache2-utils libexpat1 ssl-cert
-aptitude install -y libapache2-mod-wsgi
+aptitude install -y libapache2-mod-wsgi libapache2-mod-perl2
 service apache2 restart
 apt-get install -y subversion nodejs sqlite3 python-subversion python-sqlite
 # Install the second wave of packages (Trac).
@@ -21,6 +21,8 @@ apt-get autoremove -y
 mkdir /home/svn /home/trac
 # Set /home/node to be our project root.
 ln -s /vagrant /home/server
+# Set a symlink to the common /usr/share folder.
+ln -s /usr/lib/python2.7/dist-package/trac /usr/share/trac
 
 # Create a Trac user
 adduser --system --shell /bin/sh --gecos 'Trac Project Managment' --group --disabled-password --home /home/trac trac
@@ -28,11 +30,12 @@ adduser --system --shell /bin/sh --gecos 'Trac Project Managment' --group --disa
 adduser www-data trac
 
 # Fix permissions
+chown www-data:www-data /home/svn
 chown www-data:trac /home/trac
 chmod -R g+rws /home/svn
 chmod 0775 -R /home/trac
 
-svnserve --daemon --root /home/svn
+sudo -u www-data svnserve --daemon --root /home/svn
 
 service apache2 stop
 a2enmod rewrite
@@ -42,13 +45,25 @@ service apache2 start
 
 echo "Creating an example repository named 2014-KettleProject"
 
-# Create an example SVN repository
-mkdir /home/svn/2014/KettleProject -p
-svnadmin create /home/svn/2014/KettleProject
-cp /home/server/defaults/svnserve.conf /home/svn/2014/KettleProject/conf/svnserve.conf
-cp /home/server/defaults/passwd.ini /home/svn/2014/KettleProject/conf/passwd.ini
-# Create some example Trac projects
-mkdir /home/trac/2014/KettleProject -p
-sudo -u trac trac-admin /home/trac/2014/KettleProject initenv
-sudo -u trac trac-admin /home/trac/2014/KettleProject deploy /home/trac/2014/KettleProject/deploy
-chmod 0775 -R /home/trac
+NAME="KettleProject"
+URL=$(date +"%Y")"/$NAME"
+
+# Create the Subversion Repository
+sudo -u www-data mkdir "/home/svn/$URL" -p
+sudo -u www-data svnadmin create "/home/svn/$URL"
+sudo -u www-data cp /home/server/svn/svnserve.conf "/home/svn/$URL/conf/svnserve.conf"
+sudo -u www-data cp /home/server/svn/passwd.ini "/home/svn/$URL/conf/passwd.ini"
+
+# Create the Trac instance
+sudo -u trac mkdir "/home/trac/$URL" -p
+sudo -u trac trac-admin "/home/trac/$URL" initenv "$NAME" "sqlite:db/trac.db"
+sudo -u trac trac-admin "/home/trac/$URL" repository add "$NAME" "/home/svn/$URL"
+
+sudo -u trac trac-admin "/home/trac/$URL" permission add admin TRAC_ADMIN
+sudo -u trac trac-admin "/home/trac/$URL" permission add developer BROWSER_VIEW CHANGESET_VIEW FILE_VIEW LOG_VIEW MILESTONE_ADMIN REPORT_ADMIN SEARCH_VIEW TICKET_ADMIN TIMELINE_VIEW WIKI_ADMIN
+
+echo 'james = h3r0' >> "/home/svn/$URL/conf/passwd.ini"
+sudo -u trac trac-admin "/home/trac/$URL" permission add "james" developer
+
+sudo -u trac trac-admin "/home/trac/$URL" deploy "/home/trac/$URL/deploy"
+chmod 0775 -R "/home/trac/$URL"
--- node/bin/kentprojects.sh
+++ node/bin/kentprojects.sh
@@ -4,5 +4,64 @@
 # @license: Copyright KentProjects
 # @link: http://kentprojects.com
 
-echo "damn windows line endings!"
-exit 0
+#
+# Add a new user to a repository.
+#
+# @param string $1 The repository name
+# @param string $1 The user name
+# @return void
+#
+function AddUserToRepository
+{
+	if [ -z "$1" ]; then
+		echo "Please supply a repository name to AddUserToRepository"
+		return 1;
+	fi
+	if [ -z "$2" ]; then
+		echo "Please supply a user name to AddUserToRepository"
+		return 1;
+	fi
+	if [ ! -d "/home/svn/$1" ]; then
+		echo "Repository does not exist at /home/svn/$1"
+		return 2;
+	fi
+
+	echo "$2 = h3r0" >> "/home/svn/$URL/conf/passwd.ini"
+	sudo -u trac trac-admin "/home/trac/$URL" permission add "$2" developer
+	sudo -u trac trac-admin "/home/trac/$URL" deploy /home/trac/$URL/deploy
+	chmod 0775 -R "/home/trac/$URL"
+}
+
+#
+# Creates a new repository.
+#
+# @param string $1 The new repository name
+# @return void
+#
+function CreateRepository
+{
+	if [ -z "$1" ]; then
+		echo "Please supply a repository name to CreateRepository"
+		return 1;
+	fi
+
+	NAME="$1"
+	URL=$(date +"%Y")"/$NAME"
+
+	# Create the Subversion Repository
+	sudo -u www-data mkdir "/home/svn/$URL" -p
+	sudo -u www-data svnadmin create "/home/svn/$URL"
+	sudo -u www-data cp /home/server/svn/svnserve.conf "/home/svn/$URL/conf/svnserve.conf"
+	sudo -u www-data cp /home/server/svn/passwd.ini "/home/svn/$URL/conf/passwd.ini"
+
+	# Create the Trac instance
+	sudo -u trac mkdir "/home/trac/$URL" -p
+	sudo -u trac trac-admin "/home/trac/$URL" initenv "$NAME" "sqlite:db/trac.db"
+	sudo -u trac trac-admin "/home/trac/$URL" repository add "$NAME" "/home/svn/$URL"
+
+	sudo -u trac trac-admin "/home/trac/$URL" permission add admin TRAC_ADMIN
+	sudo -u trac trac-admin "/home/trac/$URL" permission add developer BROWSER_VIEW CHANGESET_VIEW FILE_VIEW LOG_VIEW MILESTONE_ADMIN REPORT_ADMIN SEARCH_VIEW TICKET_ADMIN TIMELINE_VIEW WIKI_ADMIN
+
+	sudo -u trac trac-admin "/home/trac/$URL" deploy "/home/trac/$URL/deploy"
+	chmod 0775 -R "/home/trac/$URL"
+}
